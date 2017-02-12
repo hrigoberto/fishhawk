a8b85f3c0fd11b21d2506400990ed16d3c6e3460
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-02-11 22:29:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var server = express();
+var port = process.env.PORT || 8080;
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', function(request, response){
+  response.sendFile('index.html', {root: __dirname + '/public/html'});
+});
+
+server.listen(port,function(){
+  console.log('I be lastnin ta pert', port);
+});
